eb4c27839d0aa06cb7811d03c37f6a93ff718206
gongxiangzhu <gongxiangzhu.km.com>
2018-08-07 15:00:24 +0800

    出事哈U

---
+++ backend/controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+
+/**
+ * Site controller
+ */
+class TestController extends Controller
+{
+    public function actionTest()
+    {
+        var_dump(444);
+        die;
+    }
+}
